c2a906d78506b1d525707b8cd0b57cd65283f0b2
mdaniel70 <mdaniel@gtrainerdemo.digitalbagoftricks.org>
2024-03-12 19:27:20 -0600

    upload files
    New versions

--- yahtzee.js
+++ yahtzee.js
@@ -1,45 +1,37 @@
 //Save all image src paths as variables
-let diceImagePath =
-  "https://cdn1.iconfinder.com/data/icons/inked/100/Dice-512.png";
-let dice1ImagePath =
-  "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_1-512.png";
-let dice2ImagePath =
-  "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_2-512.png";
-let dice3ImagePath =
-  "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_3-512.png";
-let dice4ImagePath =
-  "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_4-512.png";
-let dice5ImagePath =
-  "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png";
-let dice6ImagePath =
-  "https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_6-512.png";
-let lockImagePath = "https://simpleicon.com/wp-content/uploads/lock-2.png";
-let unlockImagePath =
-  "https://icons.iconarchive.com/icons/icons8/windows-8/512/Very-Basic-Unlock-icon.png";
+let diceImagePath = 'https://cdn1.iconfinder.com/data/icons/inked/100/Dice-512.png';
+let dice1ImagePath = 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_1-512.png';
+let dice2ImagePath = 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_2-512.png';
+let dice3ImagePath = 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_3-512.png';
+let dice4ImagePath = 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_4-512.png';
+let dice5ImagePath = 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_5-512.png';
+let dice6ImagePath = 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_6-512.png';
+let lockImagePath = 'https://simpleicon.com/wp-content/uploads/lock-2.png';
+let unlockImagePath = 'https://icons.iconarchive.com/icons/icons8/windows-8/512/Very-Basic-Unlock-icon.png';
 
 //Save all necessary elements as variables
-let dice1 = document.getElementById("dice1");
-let dice2 = document.getElementById("dice2");
-let dice3 = document.getElementById("dice3");
-let dice4 = document.getElementById("dice4");
-let dice5 = document.getElementById("dice5");
-let lock1 = document.getElementById("lock1");
-let lock2 = document.getElementById("lock2");
-let lock3 = document.getElementById("lock3");
-let lock4 = document.getElementById("lock4");
-let lock5 = document.getElementById("lock5");
-let rollButton = document.getElementById("roll-button");
-let resetButton = document.getElementById("reset-button");
-let rollCount = document.getElementById("roll-counter");
+let dice1 = document.getElementById('dice1');
+let dice2 = document.getElementById('dice2');
+let dice3 = document.getElementById('dice3');
+let dice4 = document.getElementById('dice4');
+let dice5 = document.getElementById('dice5');
+let lock1 = document.getElementById('lock1');
+let lock2 = document.getElementById('lock2');
+let lock3 = document.getElementById('lock3');
+let lock4 = document.getElementById('lock4');
+let lock5 = document.getElementById('lock5');
+let rollButton = document.getElementById('roll-button');
+let resetButton = document.getElementById('reset-button');
+let rollCount = document.getElementById('roll-counter');
 
 //Function to determine random roll for each dice. Selects five random numbers. Checks if the dice has been locked, if not then it assigns the correlating dice image path for the random number to each dice.
 const rollDice = () => {
-  let randomNum1 = Math.floor(Math.random() * 6);
-  let randomNum2 = Math.floor(Math.random() * 6);
-  let randomNum3 = Math.floor(Math.random() * 6);
-  let randomNum4 = Math.floor(Math.random() * 6);
-  let randomNum5 = Math.floor(Math.random() * 6);
-
+  let randomNum1 = Math.floor(Math.random()*6);
+  let randomNum2 = Math.floor(Math.random()*6);
+  let randomNum3 = Math.floor(Math.random()*6);
+  let randomNum4 = Math.floor(Math.random()*6);
+  let randomNum5 = Math.floor(Math.random()*6);
+  
   if (lock1.src === unlockImagePath) {
     if (randomNum1 === 0) {
       dice1.src = dice1ImagePath;
@@ -53,9 +45,9 @@ const rollDice = () => {
       dice1.src = dice5ImagePath;
     } else if (randomNum1 === 5) {
       dice1.src = dice6ImagePath;
-    }
-  }
-
+    };
+  };
+  
   if (lock2.src === unlockImagePath) {
     if (randomNum2 === 0) {
       dice2.src = dice1ImagePath;
@@ -69,9 +61,9 @@ const rollDice = () => {
       dice2.src = dice5ImagePath;
     } else if (randomNum2 === 5) {
       dice2.src = dice6ImagePath;
-    }
-  }
-
+    };
+  };
+  
   if (lock3.src === unlockImagePath) {
     if (randomNum3 === 0) {
       dice3.src = dice1ImagePath;
@@ -85,9 +77,9 @@ const rollDice = () => {
       dice3.src = dice5ImagePath;
     } else if (randomNum3 === 5) {
       dice3.src = dice6ImagePath;
-    }
-  }
-
+    };
+  };
+  
   if (lock4.src === unlockImagePath) {
     if (randomNum4 === 0) {
       dice4.src = dice1ImagePath;
@@ -101,9 +93,9 @@ const rollDice = () => {
       dice4.src = dice5ImagePath;
     } else if (randomNum4 === 5) {
       dice4.src = dice6ImagePath;
-    }
-  }
-
+    };
+  };  
+  
   if (lock5.src === unlockImagePath) {
     if (randomNum5 === 0) {
       dice5.src = dice1ImagePath;
@@ -117,25 +109,25 @@ const rollDice = () => {
       dice5.src = dice5ImagePath;
     } else if (randomNum5 === 5) {
       dice5.src = dice6ImagePath;
-    }
-  }
+    };
+  };  
 };
 
-//Setting up to make the locks clickable. Will be executed when roll button is clicked.
+//Setting up to make the locks clickable. Will be executed when roll button is clicked. 
 let counter = 0;
 let lockArray = [lock1, lock2, lock3, lock4, lock5];
-let lockClick = function (lock) {
+let lockClick = function(lock) {
   if (lock.target.src === unlockImagePath) {
     lock.target.src = lockImagePath;
   } else if (lock.target.src === lockImagePath) {
     lock.target.src = unlockImagePath;
-  }
+  };
 };
-let lockEvents = function (input) {
-  input.addEventListener("click", lockClick);
+let lockEvents = function(input) {
+  input.addEventListener('click', lockClick);
 };
-let removeLockEvents = function (input) {
-  input.removeEventListener("click", lockClick);
+let removeLockEvents = function(input) {
+  input.removeEventListener('click', lockClick);
 };
 
 //Function to click the roll button. Applies rollDice function, increases counter to track which roll you are on, and make locks clickable only on the first and second rolls.
@@ -145,11 +137,11 @@ rollButton.onclick = () => {
   rollCount.innerHTML = counter;
   if (counter === 1 || counter === 2) {
     lockArray.forEach(lockEvents);
-  }
+  };
   if (counter === 3) {
-    rollButton.style.display = "none";
+    rollButton.style.display = 'none';
     lockArray.forEach(removeLockEvents);
-  }
+  };
 };
 
 //Resets the game and puts everything back to default.
@@ -164,7 +156,7 @@ resetButton.onclick = () => {
   lock3.src = unlockImagePath;
   lock4.src = unlockImagePath;
   lock5.src = unlockImagePath;
-  rollCount.innerHTML = "";
+  rollCount.innerHTML = '';
   counter = 0;
-  rollButton.style.display = "inline-block";
+  rollButton.style.display = 'inline-block';
 };
